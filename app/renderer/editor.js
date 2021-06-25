18cac49243126e149e701c06610ecbb0f2bfd1cc
Erich Izdepski <erich.izdepski@bts-s2.com>
2021-06-25 15:02:13 -0400

    code to sense changes in editor UI

---
+++ app/renderer/editor.js
@@ -0,0 +1,18 @@
+//has listeners for the program editor UI.
+//maintains overall state for all programs (12 - 4 per sound)
+//would like to use same listeners but with some extra parameters to identify
+//which sound/program was being edited.
+
+
+//generic method to update the value for a field/control given by the label
+function update(label) {
+    let newValue = parseInt(document.getElementById(label).value, 10)
+    //display as 2 digit hex
+    console.log(newValue)
+    let hexStr = newValue.toString(16)
+    if (hexStr.length < 2) {
+        hexStr = "0" + hexStr
+    }
+    document.getElementById(label + "_display").value = hexStr
+}
+
