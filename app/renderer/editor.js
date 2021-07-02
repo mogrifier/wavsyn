1ff1b3888c67e6bcb16b50ba1b8fa8fba884a2f3
Erich Izdepski <erich.izdepski@bts-s2.com>
2021-07-02 15:12:19 -0400

    added some colorful styling and condensed the tables to give more room for other things.

--- app/renderer/editor.js
+++ app/renderer/editor.js
@@ -48,7 +48,7 @@ function setSound() {
 
     //reset dirty flag warning
     var flag = document.getElementById("dirty")
-    flag.style.color = "green";
+    flag.style.color = "green"
     flag.value = "No changes yet"
     dirty = false
 }
